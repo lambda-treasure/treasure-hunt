0a150997555862295b7c762fe5d12cdd79b44e21
Divya Nair <divnairs@gmail.com>
2019-08-26 14:42:37 -0700

    automate treasure pickup with cooldown

--- index.js
+++ index.js
@@ -3,17 +3,46 @@
 require('dotenv').config()
 const fetch = require('node-fetch')
 
-const callEndpoint = (endpoint, method, data) => {
-  fetch(`https://lambda-treasure-hunt.herokuapp.com/api/adv/${endpoint}/`, {
-    method: `${method}`,
-    headers: { Authorization: `Token ${process.env.TOKEN}` },
-    body: JSON.stringify(data)
-  })
-    .then(res => res.json())
-    .then(data => console.log('\n', data))
-    .catch(error => console.error(error))
+// Main game
+async function main() {
+  // traverse graph
+  let res = await callEndpoint('init', 'get')
+  let cooldown = res.cooldown * 1000
+
+  // take all treasures, if available
+  if (res.items && res.items.length) {
+    for (let item of res.items) {
+      setTimeout(async function() {
+        res = await callEndpoint('take', 'post', { name: `${item}` })
+        cooldown = res.cooldown * 1000
+        console.log(res)
+      }, cooldown)
+    }
+  }
 }
 
-callEndpoint('init', 'get')
-callEndpoint('status', 'post')
-callEndpoint('move', 'post', { direction: 'e' })
+// Helper functions
+async function callEndpoint(endpoint, method, data) {
+  try {
+    let res = await fetch(
+      `https://lambda-treasure-hunt.herokuapp.com/api/adv/${endpoint}/`,
+      {
+        method: `${method}`,
+        headers: { Authorization: `Token ${process.env.TOKEN}` },
+        body: JSON.stringify(data)
+      }
+    )
+    res = await res.json()
+    console.log(res)
+    return res
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+main()
+
+// callEndpoint('status', 'post')
+// callEndpoint('init', 'get')
+// callEndpoint('move', 'post', { direction: 'e' })
+// callEndpoint('take', 'post', { name: 'tiny treasure' })
