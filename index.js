bd7a13afdd4e4495b48c09ef6a87651f3b0fca97
Divya Nair <divnairs@gmail.com>
2019-08-26 12:15:01 -0700

    add body in POST

--- index.js
+++ index.js
@@ -3,15 +3,17 @@
 require('dotenv').config()
 const fetch = require('node-fetch')
 
-
-const callEndpoint = (endpoint, method) => {
+const callEndpoint = (endpoint, method, data) => {
   fetch(`https://lambda-treasure-hunt.herokuapp.com/api/adv/${endpoint}/`, {
     method: `${method}`,
-    headers: { Authorization: `Token ${process.env.TOKEN}` }
+    headers: { Authorization: `Token ${process.env.TOKEN}` },
+    body: JSON.stringify(data)
   })
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => console.log('\n', data))
     .catch(error => console.error(error))
 }
 
+callEndpoint('init', 'get')
 callEndpoint('status', 'post')
+callEndpoint('move', 'post', { direction: 'e' })
