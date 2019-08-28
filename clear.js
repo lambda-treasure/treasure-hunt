50aad3999f3b3ad4b09db9dd34141c663ef5899b
benjchand <benchand@gmail.com>
2019-08-28 10:38:08 -0700

    Persisted Map and Traveled.  Watching for Shop and Pirate.

---
+++ clear.js
@@ -0,0 +1,24 @@
+const storage = require('node-persist');
+
+async function setUpYourStorage() {
+    try {
+        await storage.init();
+        // // Clear all storage
+        await storage.clear();
+
+        // // Clear map to zero
+        // await storage.removeItem('map');
+
+        // // See your map
+        // console.log(`This is your map: ${await storage.getItem(`${process.env.NAME}'s-map`)}`) 
+
+
+
+        // await storage.setItem(`Store-Room-ID`, 1);
+    }
+    catch (err) {
+        console.error(err)
+    };
+}
+
+setUpYourStorage();
